f191dc7ff37e231ccd84cee2173b0d43ba586ab0
Ridw5456 <91797281+Ridw5456@users.noreply.github.com>
2021-10-02 18:27:32 +0100

    Add files via upload

---
+++ CS_Project/Code/main program/main_v1.py
@@ -0,0 +1,24 @@
+from tkinter import *
+import time
+import datetime as dt
+import sqlite3
+
+root=Tk()
+root.title("Assignment Organiser")
+root.geometry('1400x700')
+root.resizable(height=False, width=False)
+root.iconbitmap('notebook.ico')
+root.configure(bg='grey')
+
+def Menu():
+
+def Header():
+
+def Footer():
+
+def Calendar():
+
+def Timetable():
+    
+    
+Menu()
